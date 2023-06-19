2c0d76b3e2220c35abd43cab360d7f0ccf314cc2
MyNameIsKilleRz <you@example.com>
2023-06-19 12:19:51 +0200

    Updated defines

--- include/defines.h
+++ include/defines.h
@@ -13,6 +13,7 @@
 #define TM1637_TEMP                   // BLE doesn't need to know temp, only if overheating or not
 #define TM1637_ERR_CODE               // More info here: https://wiki.scooterhacking.org/doku.php?id=nberrorcodes
 #define TM1637_BATT
+#define TM1637_DISSABLE_CHARGING_MODE
 #define TM1637_THROTTLE_VAL
 #define TM1637_THROTTLE_IN //Raw throttle val 
 #define TM1637_THROTTLE_CALIBRATION_LOW  40  //
@@ -25,7 +26,55 @@
 #define TM1637_BUTTON_PRESSCOUNTER
 
 
-// Definitions for GPIO
+// Definitions for GPIO, thanks to https://github.com/Koxx3/SmartESC_STM32_v2/blob/vesc_comp/m365/Inc/main.h
+#define PWR_BTN_Pin GPIO_PIN_14
+#define PWR_BTN_GPIO_Port GPIOC
+#define TPS_ENA_Pin GPIO_PIN_15
+#define TPS_ENA_GPIO_Port GPIOC
+#define LED_Pin GPIO_PIN_1
+#define LED_GPIO_Port GPIOD
+#define M1_TEMPERATURE_Pin GPIO_PIN_0
+#define M1_TEMPERATURE_GPIO_Port GPIOA
+#define M1_BUS_VOLTAGE_Pin GPIO_PIN_2
+#define M1_BUS_VOLTAGE_GPIO_Port GPIOA
+#define M1_CURR_AMPL_U_Pin GPIO_PIN_3
+#define M1_CURR_AMPL_U_GPIO_Port GPIOA
+#define M1_CURR_AMPL_V_Pin GPIO_PIN_4
+#define M1_CURR_AMPL_V_GPIO_Port GPIOA
+#define M1_CURR_AMPL_W_Pin GPIO_PIN_5
+#define M1_CURR_AMPL_W_GPIO_Port GPIOA
+#define PHASE_A_V_Pin GPIO_PIN_6
+#define PHASE_A_V_GPIO_Port GPIOA
+#define PHASE_B_V_Pin GPIO_PIN_7
+#define PHASE_B_V_GPIO_Port GPIOA
+#define M1_HALL_H3_Pin GPIO_PIN_0
+#define M1_HALL_H3_GPIO_Port GPIOB
+#define PHASE_C_V_Pin GPIO_PIN_1
+#define PHASE_C_V_GPIO_Port GPIOB
+#define UART_TX_Pin GPIO_PIN_10
+#define UART_TX_GPIO_Port GPIOB
+#define UART_RX_Pin GPIO_PIN_11
+#define UART_RX_GPIO_Port GPIOB
+#define M1_PWM_UL_Pin GPIO_PIN_13
+#define M1_PWM_UL_GPIO_Port GPIOB
+#define M1_PWM_VL_Pin GPIO_PIN_14
+#define M1_PWM_VL_GPIO_Port GPIOB
+#define M1_PWM_WL_Pin GPIO_PIN_15
+#define M1_PWM_WL_GPIO_Port GPIOB
+#define M1_PWM_UH_Pin GPIO_PIN_8
+#define M1_PWM_UH_GPIO_Port GPIOA
+#define M1_PWM_VH_Pin GPIO_PIN_9
+#define M1_PWM_VH_GPIO_Port GPIOA
+#define M1_PWM_WH_Pin GPIO_PIN_10
+#define M1_PWM_WH_GPIO_Port GPIOA
+#define BRAKE_LIGHT_Pin GPIO_PIN_15
+#define BRAKE_LIGHT_GPIO_Port GPIOA
+#define M1_HALL_H1_Pin GPIO_PIN_4
+#define M1_HALL_H1_GPIO_Port GPIOB
+#define M1_HALL_H2_Pin GPIO_PIN_5
+#define M1_HALL_H2_GPIO_Port GPIOB
+// gpio
+
 #define TM1637_BAUDRATE     115200  // Half-duplex
 #define TM1637_RX_PIN
 #define TM1637_TX_PIN       // TODO Later
